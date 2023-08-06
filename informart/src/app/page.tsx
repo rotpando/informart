fc345be1c80ddf9688288ed8eb5d1974ee6a36cd
Rodrigo de Sarasqueta <78835285+rotpando@users.noreply.github.com>
2023-08-05 22:57:21 -0300

    info

--- informart/src/app/page.tsx
+++ informart/src/app/page.tsx
@@ -6,6 +6,32 @@ export default function Home() {
    
         <main className={styles.main}>
           <h1>InformARt</h1>
+
+          <h3>
+            <a href=""> ¿Quiénes somos? -</a>
+            <a href=""> Informes -</a>
+            <a href=""> Fact Checking -</a>
+            <a href=""> Recursero -</a>
+          </h3>
+
+          <h2>¿Quiénes somos?</h2>
+          <p>El Observatorio de Monitoreo y Evaluación del Cumplimiento de la Ley de Cupo Laboral Trans es una red de personas trans y no binarias dedicada a garantizar la efectiva aplicación de la Ley 27.636, conocida como la Ley de Promoción del Acceso al Empleo Formal para personas Travestis, Transexuales y Transgénero "Diana Sacayán-Lohana Berkins". Esta ley fue sancionada en Argentina en el año 2021 y establece que el sector público nacional debe reservar, al menos, el 1% de sus cargos y vacantes para personas travestis, transexuales y transgénero.  Nuestra labor se enfoca en monitorear la implementación del cupo en el sector público. Como parte de nuestro compromiso con la transparencia y la rendición de cuentas, publicamos informes regulares que elaboramos a partir de pedidos de acceso a la información pública. Estos informes brindan una visión clara y objetiva sobre el avance y los desafíos en la implementación del cupo laboral trans en el sector público, así como las áreas que requieren mayor atención para lograr una inclusión laboral plena y justa.  Nuestro objetivo es seguir ser una referencia ineludible en la evaluación del cumplimiento de los derechos de las personas trans y no binarias en Argentina, contribuyendo así a una sociedad más justa e inclusiva para todxs.</p>
+          <h2>¿Qué hacemos?</h2>
+        <ul>
+            <li>
+                Defendemos activamente la ley de cupo laboral trans y abogamos por su ampliación y mejora, asegurándonos de que sea una herramienta efectiva para promover la igualdad de oportunidades en el trabajo y la erradicación de la discriminación por identidad de género.
+            </li>
+            <li>
+                Mediante pedidos regulares de acceso a la información pública, monitoreamos el cumplimiento de las políticas y acciones de empleo inclusivo implementadas por el sector público para garantizar el acceso equitativo y no discriminatorio de personas trans y no binarias.
+            </li>
+            <li>
+                Producimos informes periódicos que contienen información detallada y exhaustiva sobre la implementación de la ley de cupo en el sector público.
+            </li>
+            <li>
+                Verificamos la precisión y veracidad de la información difundida en los medios de comunicación respecto de la implementación de la Ley de Cupo Laboral Trans.
+            </li>
+        </ul>
+
           <h2>Misión:</h2>
           <ul>
             <li>A través del análisis crítico y riguroso, buscamos identificar y denunciar las injusticias laborales</li>
@@ -22,21 +48,21 @@ export default function Home() {
         </ol>
     
         <h2>Valores:</h2>
-        <ul>
+        <ol>
             <li>Conciencia de clase</li>
             <li>Solidaridad internacional</li>
             <li>Anti asimilacionismo</li>
             <li>Antiracismo</li>
-            <li>Antipunitivismo (esto lo pondría por los antecedentes penales que muchas veces no se cumple el cupo por este punto)</li>
-        </ul>
-    
-        <ol>
+            <li>Antipunitivismo</li>
             <li>Justicia</li>
             <li>Independencia</li>
             <li>Transparencia</li>
         </ol>
     
-    
+        <h2>¿Cómo apoyar esta causa?</h2>
+
+<p>Tus aportes son fundamentales para impulsar una inclusión laboral justa y equitativa para las personas trans y no binarias en el sector público. Cada contribución que recibimos nos permite fortalecer nuestra labor de investigación, seguimiento y defensa de los derechos laborales de la comunidad trans.</p>
+<a href="">Dona aqui</a>
     
           
          
