22808e818245fcd4b46e10e431452b02f4c3de92
Rodrigo de Sarasqueta <78835285+rotpando@users.noreply.github.com>
2023-08-30 05:15:34 -0300

    ffddfs

--- infomart/app/recursero/page.tsx
+++ infomart/app/recursero/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Services = () => {
-  return <div>services</div>;
+  return (<main><br/><br/><h2>Maps and services / Recursero y mapas</h2>
+  <p>En mantenimiento / Under construction</p></main>);
 };
 
 export default Services;
